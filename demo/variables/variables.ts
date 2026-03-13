395a1e1206e9355797c52cd0dfbc3243364de8ce
Sarah Stenfert <sarah.stenfert@gmail.com>
2026-03-13 15:06:44 +1300

    Refreshing JS basics (variables)

--- demo/variables/variables.ts
+++ demo/variables/variables.ts
@@ -10,3 +10,40 @@
 // https://www.typescriptlang.org/docs/handbook/variable-declarations.html#let-declarations
 //
 
+const courseName = 'typescript'; // Always add an ; to the end of the line
+// Stick to one quote type. I.e. Single or double quotes, use consistently
+const courseName3 = `typescript`;
+// There are just strings for characters in TS. No char etc. in TS
+
+const amount = 10;
+const canUseDecimals = 10.5;
+const canUseExponents = 1e3; // 1000
+const canUseOctals = 0o777;  // can use Octals, which are used in Linux permissions
+const hexByte = 0xff;
+const binary = 0b0100001;
+const bigInt = 9000n; // need to end a bit int with 'n'
+const yes = true;
+const no = false;
+
+const missing = undefined;  // unexpectedly missing
+const empty = null; // we know it's missing
+// can't reassign const
+
+let someNum = 0;    // use if we want to reassign the value
+someNum = 1;
+someNum = 2;
+
+// Can use variable shadowing/variable scope (useful in testing)
+{
+    let someNum = 5;    // someNum is 5 within this block
+}
+
+let hello;  // can create uninitialized variables, if we want to intialize later
+// TS will give us an error if we try to use it later unassigned
+
+hello = "hi";
+hello = "hey";
+hello = "hello";
+
+// const whoops;    // can't create an uninitialized variable as we only have one
+// chance with const to initialize it
---
+++ notes/core_js_fundamentals/variables.ts
@@ -0,0 +1,8 @@
+let message = "hello";
+message = "goodbye";
+
+const message2 = "hello";
+message2 = "goodbye";
+// const values are mutable (can be changed), but we can't asign new data ot it
+
+// Don't use legacy var keyword. I.e. var message = "hello";
